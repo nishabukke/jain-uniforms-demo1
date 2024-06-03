8d0cb30ff6b154e090f465ad2b9a27e6f21f062e
Nisha Bukke <sononenisha827@gmail.com>
2024-06-03 18:45:21 +0530

    jain uniform demo 2

---
+++ script.js
@@ -0,0 +1,25 @@
+// sticky header
+
+$(window).on("scroll touchmove", function() {
+
+    if ($(document).scrollTop() > $("#sticky-wrapper").position().top) {
+      $('.sticky-area').css('width', '100%');
+      $('.sticky-area').css('max-width', '1600px');
+      $('.sticky-area').css('margin', '0px auto');
+      $('.sticky-area').css('position', 'fixed');
+      $('.sticky-area').css('top', '0px');
+      $('.sticky-area').css('background', '#fff');
+      $('.sticky-area').css('transition', 'all .3s');
+      $('.sticky-area').css('z-index', '1111');
+      $('.sticky-area').css('box-shadow', '0px 5px 20px rgba(0, 0, 0, 0.15)');
+      $('#sticky-wrapper').addClass('is-sticky');
+  
+    }
+    else{
+    $('.sticky-area').removeAttr('style');
+  
+    }
+    
+  });
+
+
