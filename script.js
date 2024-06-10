dc4640435912aa057e5e88bc8b73c0662aa39f03
Nisha Bukke <sononenisha827@gmail.com>
2024-06-10 10:25:23 +0530

    another layout

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// sticky header
+// // sticky header
 
-$(window).on("scroll touchmove", function() {
+// $(window).on("scroll touchmove", function() {
 
-    if ($(document).scrollTop() > $("#sticky-wrapper").position().top) {
-      $('.sticky-area').css('width', '100%');
-      $('.sticky-area').css('max-width', '1600px');
-      $('.sticky-area').css('margin', '0px auto');
-      $('.sticky-area').css('position', 'fixed');
-      $('.sticky-area').css('top', '0px');
-      $('.sticky-area').css('background', '#fff');
-      $('.sticky-area').css('transition', 'all .3s');
-      $('.sticky-area').css('z-index', '1111');
-      $('.sticky-area').css('box-shadow', '0px 5px 20px rgba(0, 0, 0, 0.15)');
-      $('#sticky-wrapper').addClass('is-sticky');
+//     if ($(document).scrollTop() > $("#sticky-wrapper").position().top) {
+//       $('.sticky-area').css('width', '100%');
+//       $('.sticky-area').css('max-width', '1600px');
+//       $('.sticky-area').css('margin', '0px auto');
+//       $('.sticky-area').css('position', 'fixed');
+//       $('.sticky-area').css('top', '0px');
+//       $('.sticky-area').css('background', 'rgba(0,0,0,0.7)');
+//       $('.sticky-area').css('transition', 'all .3s');
+//       $('.sticky-area').css('z-index', '1111');
+//       $('.sticky-area').css('box-shadow', '0px 5px 20px rgba(0, 0, 0, 0.15)');
+//       $('#sticky-wrapper').addClass('is-sticky');
   
-    }
-    else{
-    $('.sticky-area').removeAttr('style');
+//     }
+//     else{
+//     $('.sticky-area').removeAttr('style');
   
-    }
+//     }
     
-  });
+//   });
 
 
